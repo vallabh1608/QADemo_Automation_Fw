8b742942cd4c6028ee6f60788225aab72a1b1281
vallabh1608 <vallabhkumargarina16@gmail.com>
2026-06-19 11:26:48 +0530

    made some changes

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -1,48 +1,48 @@
-package base;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-import testUtils.ConfigReader;
-
-import java.time.Duration;
-
-public class DriverFactory {
-
-    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
-
-    public static WebDriver initDriver() {
-
-        String browser = ConfigReader.getProperty("browser").toLowerCase();
-
-        boolean isHeadless = Boolean.parseBoolean(ConfigReader.getProperty("headless"));
-        
-        switch (browser) {
-
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-
-                if (isHeadless) {
-                    chromeOptions.addArguments("--headless=new");
-                    chromeOptions.addArguments("--disable-gpu");
-                    chromeOptions.addArguments("--window-size=1920,1080");
-                }
-
-                // ✅ Jenkins stability flags
-                chromeOptions.addArguments("--no-sandbox");
-                chromeOptions.addArguments("--disable-dev-shm-usage");
-                chromeOptions.addArguments("--start-maximized");
-
-                tlDriver.set(new ChromeDriver(chromeOptions));
-                break;
-
+//package base;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.EdgeOptions;
+//
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.firefox.FirefoxOptions;
+//
+//import testUtils.ConfigReader;
+//
+//import java.time.Duration;
+//
+//public class DriverFactory {
+//
+//    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+//
+//    public static WebDriver initDriver() {
+//
+//        String browser = ConfigReader.getProperty("browser").toLowerCase();
+//
+//        boolean isHeadless = Boolean.parseBoolean(ConfigReader.getProperty("headless"));
+//        
+//        switch (browser) {
+//
+//            case "chrome":
+//                ChromeOptions chromeOptions = new ChromeOptions();
+//
+//                if (isHeadless) {
+//                    chromeOptions.addArguments("--headless=new");
+//                    chromeOptions.addArguments("--disable-gpu");
+//                    chromeOptions.addArguments("--window-size=1920,1080");
+//                }
+//
+//                // ✅ Jenkins stability flags
+//                chromeOptions.addArguments("--no-sandbox");
+//                chromeOptions.addArguments("--disable-dev-shm-usage");
+//                chromeOptions.addArguments("--start-maximized");
+//
+//                tlDriver.set(new ChromeDriver(chromeOptions));
+//                break;
+//
 //            case "edge":
 //                EdgeOptions edgeOptions = new EdgeOptions();
 //
@@ -57,10 +57,105 @@ public class DriverFactory {
 //
 //                tlDriver.set(new EdgeDriver(edgeOptions));
 //                break;
+//                
+//            case "edge":
+//                EdgeOptions edgeOptions = new EdgeOptions();
+//
+//                edgeOptions.addArguments("--disable-dev-shm-usage");
+//                edgeOptions.addArguments("--no-sandbox");
+//
+//                if (isHeadless) {
+//                    edgeOptions.addArguments("--headless=new");
+//                    edgeOptions.addArguments("--window-size=1920,1080");
+//                } else {
+//                    edgeOptions.addArguments("--start-maximized");
+//                }
+//
+//                tlDriver.set(new EdgeDriver(edgeOptions));
+//                break;
+//            case "firefox":
+//                FirefoxOptions firefoxOptions = new FirefoxOptions();
+//
+//                if (isHeadless) {
+//                    firefoxOptions.addArguments("--headless");
+//                }
+//
+//                tlDriver.set(new FirefoxDriver(firefoxOptions));
+//                tlDriver.get().manage().window().maximize();
+//                break;
+//
+//            default:
+//                throw new RuntimeException("Invalid browser: " + browser);
+//        }
+//
+//        // ✅ Common setup
+//        getDriver().manage().deleteAllCookies();
+//        getDriver().manage().timeouts().implicitlyWait(
+//                Duration.ofSeconds(Long.parseLong(ConfigReader.getProperty("implicitWait")))
+//        );
+//
+//        return getDriver();
+//    }
+//
+//    public static WebDriver getDriver() {
+//        return tlDriver.get();
+//    }
+//
+//    public static void quitDriver() {
+//        if (getDriver() != null) {
+//            getDriver().quit();
+//            tlDriver.remove();
+//        }
+//    }
+//}
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import testUtils.ConfigReader;
+
+import java.time.Duration;
+
+public class DriverFactory {
+
+    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    public static WebDriver initDriver() {
+
+        String browser = ConfigReader.getProperty("browser").toLowerCase();
+
+        // ✅ CRITICAL JENKINS FIX: Check Maven argument first, fallback to config.properties
+        String sysHeadless = System.getProperty("headless");
+        boolean isHeadless = sysHeadless != null ? Boolean.parseBoolean(sysHeadless) : Boolean.parseBoolean(ConfigReader.getProperty("headless"));
+        
+        switch (browser) {
+
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+
+                // Jenkins stability flags
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    chromeOptions.addArguments("--start-maximized");
+                }
+
+                tlDriver.set(new ChromeDriver(chromeOptions));
+                break;
                 
             case "edge":
                 EdgeOptions edgeOptions = new EdgeOptions();
 
+                // Jenkins stability flags
                 edgeOptions.addArguments("--disable-dev-shm-usage");
                 edgeOptions.addArguments("--no-sandbox");
 
@@ -73,15 +168,22 @@ public class DriverFactory {
 
                 tlDriver.set(new EdgeDriver(edgeOptions));
                 break;
+
             case "firefox":
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
 
                 if (isHeadless) {
                     firefoxOptions.addArguments("--headless");
+                    firefoxOptions.addArguments("--width=1920");
+                    firefoxOptions.addArguments("--height=1080");
                 }
 
                 tlDriver.set(new FirefoxDriver(firefoxOptions));
-                tlDriver.get().manage().window().maximize();
+                
+                // Only maximize if NOT running headless
+                if (!isHeadless) {
+                    tlDriver.get().manage().window().maximize();
+                }
                 break;
 
             default:
