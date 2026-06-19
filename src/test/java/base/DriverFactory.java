850ac30cd6a984cd3501cb8365e29b6f692059dd
vallabh1608 <vallabhkumargarina16@gmail.com>
2026-06-19 09:34:23 +0530

    updated driver factory

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -3,13 +3,10 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
 import testUtils.ConfigReader;
 
 import java.time.Duration;
@@ -21,39 +18,39 @@ public class DriverFactory {
     public static WebDriver initDriver() {
 
         String browser = ConfigReader.getProperty("browser").toLowerCase();
-
-        //boolean isHeadless = Boolean.parseBoolean(ConfigReader.getProperty("headless"));
+        
+        // Read headless from properties, but allow Jenkins to override it via Environment Variables
+        String envHeadless = System.getenv("HEADLESS");
+        boolean isHeadless = envHeadless != null ? Boolean.parseBoolean(envHeadless) : Boolean.parseBoolean(ConfigReader.getProperty("headless"));
         
         switch (browser) {
 
             case "chrome":
                 ChromeOptions chromeOptions = new ChromeOptions();
-
-//                if (isHeadless) {
-//                    chromeOptions.addArguments("--headless=new");
-//                    chromeOptions.addArguments("--disable-gpu");
-//                    chromeOptions.addArguments("--window-size=1920,1080");
-//                }
-
-                // ✅ Jenkins stability flags
                 chromeOptions.addArguments("--no-sandbox");
                 chromeOptions.addArguments("--disable-dev-shm-usage");
-                chromeOptions.addArguments("--start-maximized");
+
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    chromeOptions.addArguments("--start-maximized");
+                }
 
                 tlDriver.set(new ChromeDriver(chromeOptions));
                 break;
 
             case "edge":
                 EdgeOptions edgeOptions = new EdgeOptions();
-
-//                if (isHeadless) {
-//                    edgeOptions.addArguments("--headless=new");
-//                    edgeOptions.addArguments("--disable-gpu");
-//                    edgeOptions.addArguments("--window-size=1920,1080");
-//                }
-
-                edgeOptions.addArguments("--start-maximized");
                 edgeOptions.addArguments("--disable-dev-shm-usage");
+                edgeOptions.addArguments("--no-sandbox"); // Critical for Edge in Jenkins
+
+                if (isHeadless) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--window-size=1920,1080");
+                } else {
+                    edgeOptions.addArguments("--start-maximized");
+                }
 
                 tlDriver.set(new EdgeDriver(edgeOptions));
                 break;
@@ -61,12 +58,17 @@ public class DriverFactory {
             case "firefox":
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
 
-//                if (isHeadless) {
-//                    firefoxOptions.addArguments("--headless");
-//                }
+                if (isHeadless) {
+                    firefoxOptions.addArguments("--headless");
+                    firefoxOptions.addArguments("--width=1920");
+                    firefoxOptions.addArguments("--height=1080");
+                }
 
                 tlDriver.set(new FirefoxDriver(firefoxOptions));
-                tlDriver.get().manage().window().maximize();
+                
+                if (!isHeadless) {
+                    tlDriver.get().manage().window().maximize();
+                }
                 break;
 
             default:
